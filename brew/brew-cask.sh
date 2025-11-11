eadedbafc882f0faea5a8970ab2f90051a9a32f5
Liam Beeton <liam.beeton@gmail.com>
2025-11-11 17:47:02 +0200

    Add new brew cask packages

--- brew/brew-cask.sh
+++ brew/brew-cask.sh
@@ -5,29 +5,40 @@ set -e
 
 casks=(
   1password
+  arq
+  brave-browser
   charles
+  discord
   docker-desktop
   firefox
-  focusrite-control-2
   font-fira-code
   font-hack-nerd-font
   font-jetbrains-mono
   font-source-code-pro
   foobar2000
   google-chrome
+  imageoptim
   insomnia
   iterm2
   jetbrains-toolbox
   little-snitch
-  native-access
+  maccy
+  ngrok
+  notion
+  postman
   protonvpn
-  rekordbox
+  raycast
+  rectangle
+  slack
   spotify
   typora
   visual-studio-code
   vlc
+  whatsapp
+  wireshark-app
   xact
   yubico-authenticator
+  zoom
 )
 
 brew install --cask --force "${casks[@]}"
